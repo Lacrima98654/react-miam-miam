19fdf2a2b45c3be0c8887f5ed116b2f2ef340cdc
Lacrima98654 <romeokakpo3@gmail.com>
2021-09-04 18:50:10 +0100

    Amélioration

--- src/App.js
+++ src/App.js
@@ -28,19 +28,6 @@ const Banner = () =>(
     </React.Fragment>
 );
 
-const Info = () => (
-  <div className="p-5 info d-inline-block col-4">
-      Horaire:<br/><br/>
-      Lundi au vendredi<br/>
-      Heure d'ouverture: 09h00<br/>
-      Heure de fermerture: 22h<br/><br/>
-      Samedi - Dimanche<br/>
-      Heure d'ouverture: 09h00<br/>
-      Heure de fermerture: 02h<br/><br/>
-      Etat Actuel: Ouvert<br/>
-  </div>
-);
-
 const Main = () =>(
     <main>
         <h1 style={{textAlign:"center",color:"yellow"}}>Bar Restau Miam Miam</h1>
@@ -88,7 +75,7 @@ const About = () =>(
     <React.Fragment>
         <div id="about"  className="container pt-4">
             <h2 style={{color:"yellow"}}>A propos de nous ?</h2>
-            <h3 style={{color:"yellow"}}>Nos horaires:</h3>
+            <h3>Nos horaires:</h3>
             Lundi au vendredi<br/>
             Heure d'ouverture: 09h00<br/>
             Heure de fermerture: 22h<br/><br/>
@@ -96,6 +83,27 @@ const About = () =>(
             Heure d'ouverture: 09h00<br/>
             Heure de fermerture: 02h<br/><br/>
             Etat Actuel: Ouvert<br/>
+            <br/>
+            <h3>A propos du restaurant</h3>
+            <p className="lead">
+                Le restaurant Miam-miam créé par Santos Gari est ce que l'on
+                peut appeler une nouvelle ère des mets Africains et européens, les différents
+                menus fait de main de maître n'ont qu'un seul but: <strong>SATISFAIRES VOS
+                PAPILLES GUSTATIVES</strong>, qui que vous soyez et d'où vos venez
+                vous trouverez satistafation dans notre restaurant.
+                Notre équipe toujours prêt et 100% actif saura prendre soin du roi/ de la reine
+                que vous êtes, n'hésitez pas une seconde, votre délicieux repas est prêt
+                et n'attends que vous.
+            </p>
+            <h3>Réservation et autres</h3>
+            <p className="lead">
+                En autre le service de restauration, notre restaurant prend également en 
+                charge l'organisation de grands événements comme la réception d'un mariage,
+                un baptème et tout autre événement festif.<br/>
+                Le restaurant prend entièrement en charge la restauration😊.
+                En cas de besoin d'un restautant pour l'organisation de votre
+                fête, il n'y a qu'un simple formulaire à remplir, cliquez <a href="#shop">ici</a>. 
+            </p>
         </div>
     </React.Fragment>
 );
@@ -103,27 +111,49 @@ const About = () =>(
 class Form extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            name :"",
+            email : "",
+            tel : "",
+            msg: ""
+        }
+    }
+    handleChange(e){
+        this.setState(
+            {[e.target.name]:e.target.value}
+        )
+    }
+    handleSubmit(){
+
     }
     render(){
        return(
            <React.Fragment>
-               <form>
+               <form onSubmit={this.handleSubmit.bind(this)}>
                    <p>
                        <div className="mb-3">
                            <label htmlFor="name" className="form-label">Nom</label>
-                           <input type="text" className="form-control" id="name"/>
+                           <input type="text" name="name" className="form-control" id="name"
+                            value={this.state.name} onChange={this.handleChange.bind(this)}
+                           />
                        </div>
                        <div className="mb-3">
                            <label htmlFor="mail" className="form-label">Adresse Email</label>
-                           <input type="email" className="form-control" id="mail" placeholder="nom@exemple.com"/>
+                           <input type="email" name="email" className="form-control" id="mail" placeholder="nom@exemple.com"
+                           value={this.state.email} onChange={this.handleChange.bind(this)}
+                           />
                        </div>
                        <div className="mb-3">
                            <label htmlFor="num" className="form-label">Numéro téléphoique</label>
-                           <input type="tel" className="form-control" id="num"/>
+                           <input type="tel" name="tel" className="form-control" id="num"
+                           value={this.state.tel} onChange={this.handleChange.bind(this)}
+                           />
                        </div>
                        <div className="pb-3">
                            <label htmlFor="msg" className="form-label">Laissez votre message...</label>
-                           <textarea className="form-control" id="msg" rows="5"/>
+                           <textarea name="msg" className="form-control" id="msg" rows="5"
+                           value={this.state.msg} onChange={this.handleChange.bind(this)}
+                           />
                        </div>
                    </p>
                </form>
@@ -152,7 +182,12 @@ const Contact = () =>(
 const Shop = () =>(
     <React.Fragment>
         <div id="shop" className="container">
-
+            <h3>Commandes et Réservation</h3>
+            <p className="lead">
+                Pour toutes vos commandes et réservation vous êtes au bon endroits.
+                Une fois le formulaire ci dessous rempli, notre équipe de 
+                communication vous donnera une aussitôt dans les 15minutes qui suivront.
+            </p>
         </div>
     </React.Fragment>
 );
@@ -161,7 +196,7 @@ const Footer = () => (
     <React.Fragment>
         <footer className="container">
             <p>
-                Site Web réalisé avec Bootstrap et React JS.
+                Site Web réalisé avec Bootstrap et React JS par <a href="tel:+22991985934">Roméo KAKPO</a>.
             </p>
         </footer>
     </React.Fragment>
